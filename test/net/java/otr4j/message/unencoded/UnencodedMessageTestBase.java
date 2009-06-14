ac4a7345b4c9bf4937a9040db54e04676015c3b9
Geekius Caesar <geekius.caesar@gmail.com>
2009-06-14 16:19:42 +0200

    Removing deleted files

--- test/net/java/otr4j/message/unencoded/UnencodedMessageTestBase.java
+++
@@ -1,9 +0,0 @@
-package net.java.otr4j.message.unencoded;
-
-import junit.framework.TestCase;
-
-public abstract class UnencodedMessageTestBase extends TestCase {
-
-	public abstract void testDisassemble();
-	
-}
