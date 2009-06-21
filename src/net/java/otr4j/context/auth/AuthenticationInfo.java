e1bcba344292df2459b705a5083ec35170d996ae
Geekius Caesar <geekius.caesar@gmail.com>
2009-06-21 17:14:01 +0200

    Removed unused imports

--- src/net/java/otr4j/context/auth/AuthenticationInfo.java
+++ src/net/java/otr4j/context/auth/AuthenticationInfo.java
@@ -6,8 +6,6 @@ import java.security.InvalidKeyException;
 import java.security.KeyPair;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
-import java.security.PrivateKey;
-import java.security.PublicKey;
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
