fd1facac287a14ac265f7edd5b5797a5ffff97c4
Geekius Caesar <geekius.caesar@gmail.com>
2009-08-01 15:08:51 +0200

    Fixed build error (SessionStatus was forgotten)

---
+++ src/net/java/otr4j/SessionStatus.java
@@ -0,0 +1,13 @@
+package net.java.otr4j;
+
+public class SessionStatus {
+	private int messageState;
+
+	public void setMessageState(int messageState) {
+		this.messageState = messageState;
+	}
+
+	public int getMessageState() {
+		return messageState;
+	}
+}
