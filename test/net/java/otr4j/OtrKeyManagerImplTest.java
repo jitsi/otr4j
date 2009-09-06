08b6f9de5c9d5f8b2fc6463daf047488164af7e7
Geekius Caesar <geekius.caesar@gmail.com>
2009-09-06 18:12:46 +0200

    Initial implementation of internal key management in OtrKeyManager.

---
+++ test/net/java/otr4j/OtrKeyManagerImplTest.java
@@ -0,0 +1,17 @@
+package net.java.otr4j;
+
+import net.java.otr4j.session.SessionID;
+
+public class OtrKeyManagerImplTest extends junit.framework.TestCase {
+
+	private SessionID aliceSessionID = new SessionID("Alice@Wonderland",
+			"Bob@Wonderland", "Scytale");
+
+	public void test() throws Exception {
+		OtrKeyManager keyManager = new OtrKeyManagerImpl("C:\\otr.properties");
+		keyManager.generateLocalKeyPair(aliceSessionID);
+
+		keyManager.verify(aliceSessionID);
+		assert (keyManager.isVerified(aliceSessionID));
+	}
+}
