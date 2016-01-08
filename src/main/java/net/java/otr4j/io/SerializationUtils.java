81a8c9137f62333c0f3f57800c226a704bb07980
hoijui <hoijui.quaero@gmail.com>
2016-01-08 19:33:21 +0100

    do not use array brackets at illegal position

--- src/main/java/net/java/otr4j/io/SerializationUtils.java
+++ src/main/java/net/java/otr4j/io/SerializationUtils.java
@@ -426,10 +426,10 @@ public class SerializationUtils {
 
 	}
 
-	private static final char HEX_ENCODER[] = {'0', '1', '2', '3', '4', '5',
+	private static final char[] HEX_ENCODER = {'0', '1', '2', '3', '4', '5',
 			'6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
-	public static String byteArrayToHexString(byte in[]) {
+	public static String byteArrayToHexString(byte[] in) {
 		int i = 0;
 		if (in == null || in.length <= 0)
 			return null;
