c9e2c6100e28e1c228d79866255e6d32f3ac3761
hoijui <hoijui.quaero@gmail.com>
2016-01-14 20:36:14 +0100

    touch-up some comments and convert them to JavaDocs in TLV

--- src/main/java/net/java/otr4j/session/TLV.java
+++ src/main/java/net/java/otr4j/session/TLV.java
@@ -9,19 +9,18 @@ package net.java.otr4j.session;
  */
 public class TLV {
 
-	/* This is just padding for the encrypted message, and should be ignored. */
+	/** This is just padding for the encrypted message, and should be ignored. */
 	public static final int PADDING = 0;
-	/* The sender has thrown away his OTR session keys with you */
+	/** The sender has thrown away his OTR session keys with you */
 	public static final int DISCONNECTED = 0x0001;
 
-	/* The message contains a step in the Socialist Millionaires' Protocol. */
+	/* The message contains a step in the Socialist Millionaire's Protocol. */
 	public static final int SMP1 = 0x0002;
 	public static final int SMP2 = 0x0003;
 	public static final int SMP3 = 0x0004;
 	public static final int SMP4 = 0x0005;
 	public static final int SMP_ABORT = 0x0006;
-	/* Like OTRL_TLV_SMP1, but there's a question for the buddy at the
-	 * beginning */
+	/** Like {@link #SMP1}, but there's a question for the buddy at the beginning. */
 	public static final int SMP1Q = 0x0007;
 
 	int type;
